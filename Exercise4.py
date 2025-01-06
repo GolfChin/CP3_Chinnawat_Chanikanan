5bc1a091015d67b31a77d56bb2b38f9f83030fc8
GolfChin <golfchin@gmail.com>
2025-01-06 22:32:55 +0700

    Exercise4

---
+++ Exercise4.py
@@ -0,0 +1,12 @@
+print ("--- Your Score ---")
+
+sEnglish =(2.5)
+sBusiness =(3.2)
+sComSys =(3.5)
+sComProg =(3.8)
+
+print ("Foundation English : ",sEnglish)
+print ("General Business : ",sBusiness)
+print ("Introduction to Computer Systems : ",sComSys)
+print ("Computer Programming : ",sComProg)
+ 

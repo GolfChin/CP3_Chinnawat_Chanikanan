8df3c74ae1f4e88d6e855d8e95b2bb79c65b2fc1
GolfChin <golfchin@gmail.com>
2025-01-13 14:05:38 +0700

    Excercisee5_1

---
+++ Exercise5_1_Chinawat_C.py
@@ -0,0 +1,8 @@
+gInput1 = input("Input1 :")
+gInput2 = input("Input2 :")
+
+print (gInput1,"+",gInput2,"=",int(gInput1)+int(gInput2))
+print (gInput1,"-",gInput2,"=",int(gInput1)-int(gInput2))
+print (gInput1,"*",gInput2,"=",int(gInput1)*int(gInput2))
+print (gInput1,"/",gInput2,"=",int(gInput1)/int(gInput2))
+
